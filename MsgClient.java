8f5d0fe7912d4770fc98ede45b4854eb229111bb
JackTedo <zliu@zons-MacBook-Pro.local>
2018-09-05 13:41:58 -0400

    Update

--- MsgClient.java
+++ MsgClient.java
@@ -1,34 +1,21 @@
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class MsgClient {
-    private Socket socket;
-    private Scanner scanner;
 
-    private MsgClient(InetAddress serverAddress, int serverPort) throws Exception {
-	this.socket = new Socket(serverAddress, serverPort);
-	this.scanner = new Scanner(System.in);
-    }
-
-    private void start() throws IOException {
-	String input;
-	while (true) {
-	    input = scanner.nextLine();
-	    PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-	    out.println(input);
-	    out.flush();
-	}
-    }
+    private static Socket socket;
 
     public static void main(String[] args) throws Exception {
-	MsgClient client = new MsgClient(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
 
-	System.out.println("\r\nConnected: " + client.socket.getInetAddress());
-	client.start();
+	socket = new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+
+	System.out.println("\r\nConnected.");
+
+	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+	out.println(args[2]);
+
     }
 
 }
